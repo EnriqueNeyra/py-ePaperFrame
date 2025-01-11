31a55f5235471c203808ccd215de452f6a6248ff
EnriqueNeyra <94760483+EnriqueNeyra@users.noreply.github.com>
2025-01-11 12:14:22 -0800

    changes to setup script

--- setup.sh
+++ setup.sh
@@ -9,66 +9,41 @@ sudo sed -i 's/^dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo sed -i 's/^#dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo raspi-config nonint do_i2c 0
 
-## venv and dependency setup
-#echo "Setting up virtual environment..."
-#python3 -m venv venv
-#source venv/bin/activate
-
-#echo "Installing packages from requirements.txt..."
-#pip3 install -r requirements.txt
-#deactivate
-#echo "Packages installed."
-
+echo "Installing required pythonh package(s)"
 sudo apt-get install python3-qrcode
+sudo apt-get install python3-flask
 
 echo "Setting up python script epaper service"
-SERVICE_NAME="epaper"
 PYTHON_SCRIPT="web_server.py"
-SERVICE_PATH="/etc/systemd/system/$SERVICE_NAME.service"
+SERVICE_NAME="epaper.service"
+SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}"
 CURRENT_USER=${SUDO_USER:-$(whoami)}
 
-echo $CURRENT_USER
-
-sudo bash -c "cat > $SERVICE_PATH" <<EOL
+sudo tee "$SERVICE_PATH" > /dev/null <<EOF
 [Unit]
 Description=Flask Web Server
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 /home/enriquepi/repos/py-ePaperFrame/web_server.py
+ExecStart=/usr/bin/python3 $(pwd)/web_server.py
 WorkingDirectory=$(pwd)
 Restart=always
 User=$CURRENT_USER
 
 [Install]
 WantedBy=multi-user.target
-EOL
-
-#sudo bash -c "cat > $SERVICE_PATH" <<EOL
-#[Unit]
-#Description=My Python Script Service
-#After=network.target
-
-#[Service]
-#Type=simple
-#WorkingDirectory=$(pwd)
-#ExecStart=$(pwd)/venv/bin/python3 $(pwd)/$PYTHON_SCRIPT
-#Restart=always
-#User=$CURRENT_USER
-
-#[Install]
-#WantedBy=multi-user.target
-#EOL
+EOF
 
 sudo systemctl daemon-reload
-sudo systemctl enable $SERVICE_NAME
-#sudo systemctl start $SERVICE_NAME
+sudo systemctl start "$SERVICE_NAME"
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl status "$SERVICE_NAME"
 
 echo "Setup complete!"
-read -p "Reboot requried. Reboot now? (y/n): " REBOOT_CHOICE
-if [[ "$REBOOT_CHOICE" == "y" || "$REBOOT_CHOICE" == "Y" ]]; then
-    echo "Rebooting now..."
-    sudo reboot
-else
-    echo "Reboot skipped. Please remember to reboot at a later time."
-fi
+#read -p "Reboot requried. Reboot now? (y/n): " REBOOT_CHOICE
+#if [[ "$REBOOT_CHOICE" == "y" || "$REBOOT_CHOICE" == "Y" ]]; then
+    #echo "Rebooting now..."
+    #sudo reboot
+#else
+    #echo "Reboot skipped. Please remember to reboot at a later time."
+#fi
