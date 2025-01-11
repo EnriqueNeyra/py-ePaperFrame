055fe103e3ed366a49e3864d02bacc145c800944
EnriqueNeyra <94760483+EnriqueNeyra@users.noreply.github.com>
2025-01-11 12:35:41 -0800

    prvent user from starting the frame if no pictures exist. Cleanup setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# enable SPI Interface
 echo "Enabling SPI interface..."
 sudo sed -i 's/^dtparam=spi=.*/dtparam=spi=on/' /boot/config.txt
 sudo sed -i 's/^#dtparam=spi=.*/dtparam=spi=on/' /boot/config.txt
@@ -9,11 +8,11 @@ sudo sed -i 's/^dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo sed -i 's/^#dtparam=i2c_arm=.*/dtparam=i2c_arm=on/' /boot/config.txt
 sudo raspi-config nonint do_i2c 0
 
-echo "Installing required pythonh package(s)"
+echo "Installing required pythonh package(s)..."
 sudo apt-get install python3-qrcode
 sudo apt-get install python3-flask
 
-echo "Setting up python script epaper service"
+echo "Setting up python script epaper service..."
 PYTHON_SCRIPT="web_server.py"
 SERVICE_NAME="epaper.service"
 SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}"
@@ -34,16 +33,15 @@ User=$CURRENT_USER
 WantedBy=multi-user.target
 EOF
 
+echo "Enabling service..."
 sudo systemctl daemon-reload
-sudo systemctl start "$SERVICE_NAME"
 sudo systemctl enable "$SERVICE_NAME"
-sudo systemctl status "$SERVICE_NAME"
 
 echo "Setup complete!"
-#read -p "Reboot requried. Reboot now? (y/n): " REBOOT_CHOICE
-#if [[ "$REBOOT_CHOICE" == "y" || "$REBOOT_CHOICE" == "Y" ]]; then
-    #echo "Rebooting now..."
-    #sudo reboot
-#else
-    #echo "Reboot skipped. Please remember to reboot at a later time."
-#fi
+read -p "Reboot requried. Reboot now? (y/n): " REBOOT_CHOICE
+if [[ "$REBOOT_CHOICE" == "y" || "$REBOOT_CHOICE" == "Y" ]]; then
+    echo "Rebooting now..."
+    sudo reboot
+else
+    echo "Reboot skipped. Please remember to reboot at a later time."
+fi
